c66caf2f5b5f4f26cff60eaa6b899f83e171604a
Robson <robsonbrentan@gmail.com>
2021-05-29 13:26:56 -0300

    update js

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+             {
+                numero: 38111,
+                nome: 'Christianne Varão',
+                partido: 'PEN',
+                foto: [
+                    {URL: 'v1.jpg', legenda: 'Vereador'}
+                ]
+            },
+             {
+                numero: 77222,
+                nome: 'Deutrano',
+                partido: 'OUTRO',
+                foto: [
+                    {URL: 'v2', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },    
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+             {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Jon',
+                foto: [
+                    {URL: 'cp1.jpg', legenda: 'Prefeito'},
+                    {URL: 'cv1.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 77,
+                nome: 'Beltrano',
+                partido: 'UNI',
+                vice: 'Bely',
+                foto: [
+                    {URL: 'cp2.jpg', legenda: 'Prefeito'},
+                    {URL: 'cv2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    },
+]
