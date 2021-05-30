df736009e5c436cf94ef7facb96a2ac0e75b21cc
Robson <robsonbrentan@gmail.com>
2021-05-29 23:03:48 -0300

    update
    Finalização da parte lógica com JS

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ let numeros = document.querySelector ('.d-1-3');
 
 let etapaAtual = 0;
 let numero ='';
+let votoBranco = false;
+let votos = [];
 
 function comecarEtapa (){
-    let etapa = etapas[etapaAtual];
+        let etapa = etapas[etapaAtual];
 
-    let numeroHtml = '';
+        let numeroHtml = '';
+        numero = '';
+        votoBranco = false;
 
     for (let i=0;i<etapa.numeros;i++){
         if (i===0){
@@ -30,8 +34,36 @@ function comecarEtapa (){
 }
 
 function atualizaInterface(){
-    console.log ("atualizando interface");
-    console.log (numero);
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if(candidato.length > 0){
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        if (etapa.titulo === "VEREADOR"){
+        descricao.innerHTML = `Nome: ${candidato.nome}</br>Partido: ${candidato.partido} `;
+    }else {
+        descricao.innerHTML = `Nome: ${candidato.nome}</br>vice: ${candidato.vice}</br>Partido: ${candidato.partido} `;
+    } 
+        let fotosHtml = '';
+        for(let i in candidato.fotos) {
+            if (candidato.fotos[i].small){
+                fotosHtml += `<div class="d-1-image small"><img src="img/${candidato.fotos[i].URL}" alt=""/>${candidato.fotos[i].legenda}</div>`;
+            } else{
+                fotosHtml += `<div class="d-1-image"><img src="img/${candidato.fotos[i].URL}" alt=""/>${candidato.fotos[i].legenda}</div>`;}
+        }
+        lateral.innerHTML = fotosHtml;
+    } else {
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso-grande pisca">VOTO NULO</div>';
+    };
 }
 
 function clicou(n) {
@@ -46,16 +78,49 @@ function clicou(n) {
     }else {
         atualizaInterface()
     }
+    (new Audio('audio/se1.mp3')).play();
     }
 }
 function branco(){
-    alert("Clicou em BRANCO!");
+    numero = '';
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso-grande pisca">VOTO EM BRANCO</div>';
+        lateral.innerHTML= '';
+        (new Audio('audio/se1.mp3')).play();
 }
 function corrige(){
-    alert("Clicou em CORRIGE!");
+    comecarEtapa();
 }
 function confirma(){
-    alert("Clicou em CONFIRMA!");
-}
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false
 
+    if(votoBranco === true){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        })
+    } else if (numero.length === etapa.numeros){
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        })
+    }
+
+    if(votoConfirmado){
+        etapaAtual++;
+        if(etapas[etapaAtual] !== undefined){
+            comecarEtapa();
+        } else {
+            document.querySelector ('.tela').innerHTML = '<div class="aviso-gigante pisca">FIM</div>';  
+            console.log (votos);  
+        }
+    }
+    (new Audio('audio/se3.mp3')).play();
+}
 comecarEtapa ()
--- etapas.js
+++ etapas.js
@@ -4,19 +4,19 @@ let etapas = [
         numeros: 5,
         candidatos: [
              {
-                numero: 38111,
-                nome: 'Christianne Varão',
+                numero: '38111',
+                nome: 'Fernando de Mello',
                 partido: 'PEN',
-                foto: [
-                    {URL: 'v1.jpg', legenda: 'Vereador'}
+                fotos: [
+                    {URL:'v1.jpg', legenda:'Vereador'}
                 ]
             },
              {
-                numero: 77222,
-                nome: 'Deutrano',
-                partido: 'OUTRO',
-                foto: [
-                    {URL: 'v2', legenda: 'Vereador'}
+                numero: '77222',
+                nome: 'Chico do Serrado',
+                partido: 'LOL',
+                fotos: [
+                    {URL:'v2.jpg', legenda:'Vereador'}
                 ]
             },
         ]
@@ -26,23 +26,43 @@ let etapas = [
         numeros: 2,
         candidatos: [
              {
-                numero: 99,
-                nome: 'Ciclano',
+                numero: '99',
+                nome: 'Amauri Duarte',
                 partido: 'ABC',
-                vice: 'Jon',
-                foto: [
+                vice: 'Joana Maranhão',
+                fotos: [
                     {URL: 'cp1.jpg', legenda: 'Prefeito'},
                     {URL: 'cv1.jpg', legenda: 'Vice-Prefeito', small: true}
                 ]
             },
             {
-                numero: 77,
-                nome: 'Beltrano',
+                numero: '77',
+                nome: 'Val Costa',
                 partido: 'UNI',
-                vice: 'Bely',
-                foto: [
+                vice: 'Roberto Carlos',
+                fotos: [
                     {URL: 'cp2.jpg', legenda: 'Prefeito'},
                     {URL: 'cv2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '17',
+                nome: 'Cauã Brentan',
+                partido: 'FOX',
+                vice: 'Pai do Cauã',
+                fotos: [
+                    {URL: 'cp_caua.jpg', legenda: 'Prefeito'},
+                    {URL: 'cv_robs.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Cibele do Sapopemba',
+                partido: 'PT',
+                vice: 'Daniel Azevedo',
+                fotos: [
+                    {URL: 'cp_cib.jpg', legenda: 'Prefeito'},
+                    {URL: 'cv_dani.jpg', legenda: 'Vice-Prefeito', small: true}
                 ]
             },
         ]
